c636e6376df8739b0150eb57dc0845de54cd36c6
Jonas Pereira <129891766+jpereira55@users.noreply.github.com>
2025-04-12 19:26:55 -0300

    Atualizar o pdvs.js

--- pdvs.js
+++ pdvs.js
@@ -288,7 +288,7 @@ let pdvsJson = [
         name: 'PDV 08',
         descripion:'Area Nova',
         img: 'imagens/1.jpg',
-        link: 'http://192.168.60.68:9898/normal.html'
+        link: 'http://192.168.60.161:9898/normal.html'
     },
     {
         name: 'PDV 09',
