c3f496e7b751a0d85f4f064aa6fe52e6999791b8
ezfinge <73872184+ezfinge@users.noreply.github.com>
2025-06-10 14:13:49 -0300

    Update pdvs.js
    cx 18

--- pdvs.js
+++ pdvs.js
@@ -105,7 +105,7 @@ let pdvsJson = [
         name: 'PDV 18',
         descripion:'Frente de Loja',
         img: 'imagens/1.jpg',
-        link: 'http://192.168.60.49:9898/normal.html'
+        link: 'http://192.168.60.111:9898/normal.html'
     },
     {
         name: 'PDV 19',
